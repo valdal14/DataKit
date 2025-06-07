4423aa5b4e1c3f8f42c4b81be1cd4d21fb7c5e62
Valerio DAlessio <u105550@canda.com>
2025-06-07 09:41:47 +0200

    change concurrency level, swift language version and wired the tests

---
+++ DataKit/DataKit/DataKit.h
@@ -0,0 +1,18 @@
+//
+//  DataKit.h
+//  DataKit
+//
+//  Created by Valerio D'ALESSIO on 7/6/25.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for DataKit.
+FOUNDATION_EXPORT double DataKitVersionNumber;
+
+//! Project version string for DataKit.
+FOUNDATION_EXPORT const unsigned char DataKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <DataKit/PublicHeader.h>
+
+
---
+++ DataKit/DataKitTests/DataKitTests.swift
@@ -0,0 +1,18 @@
+//
+//  DataKitTests.swift
+//  DataKitTests
+//
+//  Created by Valerio D'ALESSIO on 7/6/25.
+//
+
+import Testing
+@testable import DataKit
+
+struct DataKitTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+		#expect(true)
+    }
+
+}

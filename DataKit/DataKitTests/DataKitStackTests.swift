0527856774659cde14f5c56130bb670c216b0e82
Valerio DAlessio <u105550@canda.com>
2025-06-09 09:54:45 +0200

    `DataKitActorStack` push successfully a new element into the stack

--- DataKit/DataKitTests/DataKitStackTests.swift
+++ DataKit/DataKitTests/DataKitStackTests.swift
@@ -8,19 +8,46 @@
 import Testing
 import DataKit
 
-actor DataKitActorStack<T: DataKitCompatible>: Sendable {
-	private var data: [T] = []
+public actor DataKitActorStack<T: DataKitCompatible>: Sendable {
+	private var data: DataKitActorLinkedList<T> = DataKitActorLinkedList<T>()
+	
+	public func push(_ element: T) async {
+		await data.add(element)
+	}
+	
+	public func isEmpty() async -> Bool {
+		return await data.isEmpty()
+	}
 }
 
 struct DataKitStackTests {
 
-    @Test func example() async throws {
-        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
-		#expect(true)
+    @Test("push a new user into the stack")
+	func push() async throws {
+		let sut = makeSUT()
+		let newUser = User(id: UUID(), name: "John", surname: "Doe", age: 30)
+		await sut.push(newUser)
+		let isStackEmpty = await sut.isEmpty()
+		#expect(isStackEmpty == false)
     }
 	
 	// MARK: - Helper methods
-	private func makeSUT() -> DataKitActorStack<Int> {
+	private func makeSUT() -> DataKitActorStack<User> {
 		return .init()
+	}
+}
+
+// MARK: - Custom type with conformance to the DataKitCompatible
+struct User: DataKitCompatible, Identifiable {
+	let id: UUID
+	let name: String
+	let surname: String
+	let age: Int
+	
+	init(id: UUID, name: String, surname: String, age: Int) {
+		self.id = id
+		self.name = name
+		self.surname = surname
+		self.age = age
 	}
 }
